aa8a513da01c3e3cf82400e246c5d42d5ec1842e
bosquealves <bosquealves@gmail.com>
2025-10-14 16:02:59 +0200

    Script für RLink Paket: Aufruf von NetLogo 7.0.0 Experimenten aus RStudio heraus.
    Läuft noch micht weil tmp/var Fehler auftritt. Ist ein Mac-Problem, das mit Schreibrechten zu tun hat.

---
+++ NetLogoExperimente_aus_R.r
@@ -0,0 +1,48 @@
+# Starte NetLogo (ab Version 7.00) Experimente aus RStudio
+# https://forum.netlogo.org/t/logolink-an-interface-for-running-netlogo-simulations-from-r/648
+
+library(logolink)
+
+netlogo_path <- file.path("", "Applications", "NetLogo 7.0.0", "NetLogo 7.0.0.app")
+
+model_path <- file.path(
+  "", "Applications", "NetLogo 7.0.0", "models", "IABM Textbook", "chapter 4",
+  "Wolf Sheep Simple 5.nlogox"
+)
+
+setup_file <- create_experiment(
+  name = "Wolf Sheep Simple Model Analysis",
+  repetitions = 10,
+  sequential_run_order = TRUE,
+  run_metrics_every_step = TRUE,
+  setup = "setup",
+  go = "go",
+  time_limit = 1000,
+  metrics = c(
+    'count wolves',
+    'count sheep'
+  ),
+  run_metrics_condition = NULL,
+  constants = list(
+    "number-of-sheep" = 500,
+    "number-of-wolves" = list(
+      first = 5,
+      step = 1,
+      last = 15
+    ),
+    "movement-cost" = 0.5,
+    "grass-regrowth-rate" = 0.3,
+    "energy-gain-from-grass" = 2,
+    "energy-gain-from-sheep" = 5
+  )
+)
+
+results <- run_experiment(
+  netlogo_path = netlogo_path,
+  model_path = model_path,
+  setup_file = setup_file
+)
+
+# brings error message while trying to write a temporrary file in MacOS
+# Link to potential solution: https://github.com/ocaml/opam/issues/3576
+
